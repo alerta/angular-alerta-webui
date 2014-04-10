4daec5eefc1dffb203171654d57dc8293b57329e
Nick Satterly <nick.satterly@guardian.co.uk>
2014-04-10 20:03:04 +0100

    first attempt

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,12 @@ angular.module('myApp.controllers', [])
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+
+  .controller('GetAlerts', ['$scope', 'Alerts', function($scope, Alerts){
+    Alerts.query(function(response){
+      $scope.status = response.status;
+      $scope.alerts = response.alerts;
+    })
+
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,8 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngResource'])
+  .factory('Alerts', ['$resource', function($resource) {
+    return $resource('http://localhost:8080/api/alerts', {}, {'query': {method: 'GET', isArray: false}})
+  }])
+  .value('version', '0.1');
