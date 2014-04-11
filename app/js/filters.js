93f4b1c6abf3b8ffebe3f51a6a0a4ca8bf445577
Nick Satterly <nick.satterly@guardian.co.uk>
2014-04-11 11:39:02 +0100

    fix attributes

--- app/js/filters.js
+++ app/js/filters.js
@@ -9,3 +9,9 @@ alertaFilters.filter('interpolate', ['version', function(version) {
       return String(text).replace(/\%VERSION\%/mg, version);
     };
   }]);
+
+alertaFilters.filter('capitalize', function() {
+  return function(text) {
+    return String(text).replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); });
+  };
+});
