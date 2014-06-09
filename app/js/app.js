b694722ec88fdc9964a92b1b86ea251cf37f0460
Nick Satterly <nick.satterly@guardian.co.uk>
2014-06-09 22:29:39 +0100

    first attempt

--- app/js/app.js
+++ app/js/app.js
@@ -7,7 +7,8 @@ var alertaApp = angular.module('alertaApp', [
   'alertaFilters',
   'alertaServices',
   'alertaDirectives',
-  'alertaControllers'
+  'alertaControllers',
+  'googleOauth'
 ])
 
 alertaApp.config(['$routeProvider',
@@ -35,8 +36,26 @@ alertaApp.config(['$routeProvider',
       templateUrl: 'partials/about.html',
       controller: 'AboutController'
     })
+    .when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'LoginController'
+    })
     .otherwise({
       redirectTo: '/alerts'
     });
   }]);
 
+alertaApp.config(
+  function(TokenProvider) {
+    // Demo configuration for the "angular-oauth demo" project on Google.
+    // Log in at will!
+
+    // Sorry about this way of getting a relative URL, powers that be.
+    var baseUrl = document.URL.replace('example/demo.html', '');
+
+    TokenProvider.extendConfig({
+      clientId: '191261111313.apps.googleusercontent.com',
+      redirectUri: baseUrl + 'src/oauth2callback.html',  // allow lunching demo from a mirror
+      scopes: ["https://www.googleapis.com/auth/userinfo.email"]
+    });
+  });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -401,3 +401,34 @@ alertaControllers.controller('AboutController', ['$scope', '$timeout', 'Manageme
     });
 
   }]);
+
+alertaControllers.controller('LoginController',
+  function($rootScope, $scope, $window, Token) {
+    $scope.accessToken = Token.get();
+
+    $scope.authenticate = function() {
+      var extraParams = $scope.askApproval ? {approval_prompt: 'force'} : {};
+      Token.getTokenByPopup(extraParams)
+        .then(function(params) {
+          // Success getting token from popup.
+
+          // Verify the token before setting it, to avoid the confused deputy problem.
+          Token.verifyAsync(params.access_token).
+            then(function(data) {
+              $rootScope.$apply(function() {
+                $scope.accessToken = params.access_token;
+                $scope.expiresIn = params.expires_in;
+
+                Token.set(params.access_token);
+              });
+            }, function() {
+              alert("Failed to verify token.")
+            });
+
+        }, function() {
+          // Failure getting token from popup.
+          alert("Failed to get token from popup.");
+        });
+    };
+  });
+
