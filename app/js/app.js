7af9715032b6102c7b33afd948651d51779bdf80
Nick Satterly <nick.satterly@guardian.co.uk>
2014-06-20 16:55:18 +0100

    filter alert text thru linky

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 var alertaApp = angular.module('alertaApp', [
   'ngRoute',
+  'ngSanitize',
   'alertaFilters',
   'alertaServices',
   'alertaDirectives',
