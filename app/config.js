34c55cb006841ad865664ca266d0ed87db8accec
Nick Satterly <nick.satterly@guardian.co.uk>
2014-08-03 20:23:24 +0100

    use generic defaults in config.js that should be replaced

--- app/config.js
+++ app/config.js
@@ -6,10 +6,9 @@ angular.module('config', [])
 
     'endpoint'    : "http://"+window.location.hostname+":8080",
 
-    'client_id'   : '379647311730-hn94fk7lss64ohvs1ddc01sauuspeeea.apps.googleusercontent.com',
+    'client_id'   : "CLIENT_ID",
 
-    //'redirect_url': "http://"+window.location.hostname+"/oauth2callback.html"
-    'redirect_url': "http://"+window.location.hostname+"/~nsatterl/angular-alerta-webui/app/oauth2callback.html"
+    'redirect_url': "REDIRECT_URL" // eg. "http://"+window.location.hostname+"/oauth2callback.html"
 
   });
 
