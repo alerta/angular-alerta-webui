6678c64ce32e68b07ceda4a39543f5228e759d30
Nick Satterly <nick.satterly@guardian.co.uk>
2014-09-21 11:06:42 +0100

    update endpoint URL to version 3.2

--- app/js/services.js
+++ app/js/services.js
@@ -6,55 +6,55 @@ var alertaServices = angular.module('alertaServices', ['config', 'ngResource']);
 
 alertaServices.factory('Count', ['$resource', 'config',
   function($resource, config) {
-    return $resource(config.endpoint+'/api/alerts/count', {}, {
+    return $resource(config.endpoint+'/alerts/count', {}, {
       'query': {method:'GET'}
     });
   }]);
 
 alertaServices.factory('Alert', ['$resource', 'config',
   function($resource, config) {
-    return $resource(config.endpoint+'/api/alert/:id', {}, {
-      'query':  {method:'GET', url: config.endpoint+'/api/alerts'},
+    return $resource(config.endpoint+'/alert/:id', {}, {
+      'query':  {method:'GET', url: config.endpoint+'/alerts'},
       'save':   {method:'POST'},
       'get':    {method:'GET'},
-      'status': {method:'POST', url: config.endpoint+'/api/alert/:id/status'},
+      'status': {method:'POST', url: config.endpoint+'/alert/:id/status'},
       'remove': {method:'DELETE'},
       'delete': {method:'DELETE'},
-      'tag':    {method:'POST', url: config.endpoint+'/api/alert/:id/tag'},
-      'untag':  {method:'POST', url: config.endpoint+'/api/alert/:id/untag'},
-      'top10':  {method:'GET', url: config.endpoint+'/api/alerts/top10'}
+      'tag':    {method:'POST', url: config.endpoint+'/alert/:id/tag'},
+      'untag':  {method:'POST', url: config.endpoint+'/alert/:id/untag'},
+      'top10':  {method:'GET', url: config.endpoint+'/alerts/top10'}
     });
   }]);
 
 alertaServices.factory('Environment', ['$resource', 'config',
   function($resource, config) {
-    return $resource(config.endpoint+'/api/environments?status=open', {}, {
+    return $resource(config.endpoint+'/environments?status=open', {}, {
       'all':  {method: 'GET'},
     });
   }]);
 
 alertaServices.factory('Service', ['$resource', 'config',
   function($resource, config) {
-    return $resource(config.endpoint+'/api/services', {}, {
+    return $resource(config.endpoint+'/services', {}, {
       'all':  {method: 'GET'},
     });
   }]);
 
 alertaServices.factory('Users', ['$resource', 'config',
   function($resource, config) {
-    return $resource(config.endpoint+'/api/user', {}, {
-      'query':  {method:'GET', url: config.endpoint+'/api/users'},
+    return $resource(config.endpoint+'/user', {}, {
+      'query':  {method:'GET', url: config.endpoint+'/users'},
       'save':   {method:'POST'},
-      'delete': {method:'DELETE', url: config.endpoint+'/api/user/:user'}
+      'delete': {method:'DELETE', url: config.endpoint+'/user/:user'}
     });
   }]);
 
 alertaServices.factory('Keys', ['$resource', 'config',
   function($resource, config) {
-    return $resource(config.endpoint+'/api/key', {}, {
-      'query':  {method:'GET', url: config.endpoint+'/api/keys/:user'},
+    return $resource(config.endpoint+'/key', {}, {
+      'query':  {method:'GET', url: config.endpoint+'/keys/:user'},
       'save':   {method:'POST'},
-      'delete': {method:'DELETE', url: config.endpoint+'/api/key/:key'}
+      'delete': {method:'DELETE', url: config.endpoint+'/key/:key'}
     });
   }]);
 
@@ -86,7 +86,7 @@ alertaServices.factory('Profile', [
 
 alertaServices.factory('Heartbeat', ['$resource', 'config',
   function($resource, config) {
-    return $resource(config.endpoint+'/api/heartbeats', {}, {
+    return $resource(config.endpoint+'/heartbeats', {}, {
       'query':  {method:'GET'}
     });
   }]);
