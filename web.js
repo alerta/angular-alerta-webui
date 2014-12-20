afee48b473027ad7875916ec792c15f9900deca8
Nick Satterly <nick.satterly@guardian.co.uk>
2014-12-20 14:00:00 +0000

    dynamically generate config.js

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
 
+app.get('/config.js', function(request, response) {
+
+  var config = " \
+    'use strict'; \
+    angular.module('config', []) \
+      .constant('config', { \
+        'endpoint'    : '" + process.env.ALERTA_ENDPOINT + "', \
+        'client_id'   : '" + process.env.CLIENT_ID + "', \
+        'redirect_url': '" + process.env.REDIRECT_URL + "' \
+      });";
+
+  response.send(config);
+});
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/app'));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
