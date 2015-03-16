ba50047d7191b123319148d2d699e537e884f141
Nick Satterly <nick.satterly@guardian.co.uk>
2015-03-16 16:09:42 +0000

    make config.js generic

--- app/config.js
+++ app/config.js
@@ -2,9 +2,7 @@
 
 angular.module('config', [])
   .constant('config', {
-    // 'endpoint'    : "http://"+window.location.hostname+":8080",
-    'endpoint'    : "http://localhost:8080",
-    'provider'    : "github", // google or github
-    // 'client_id'   : "379647311730-sj130ru952o3o7ig8u0ts8np2ojivr8d.apps.googleusercontent.com"
-    'client_id'   : "1ff8c23b32608d8a06bc"
+    'endpoint'    : "http://"+window.location.hostname+":8080",
+    'provider'    : "google", // google, github or twitter
+    'client_id'   : "INSERT-CLIENT-ID-HERE"
   });
