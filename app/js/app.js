c5372e375416f414e5e4d9da1491ca3a98557708
Nick Satterly <nick.satterly@guardian.co.uk>
2015-09-16 18:23:04 +0100

    angular style guide

--- app/js/app.js
+++ app/js/app.js
@@ -1,108 +1,108 @@
 'use strict';
 
+angular
+  .module('alertaApp', [
+    'config',
+    'ngRoute',
+    'ngSanitize',
+    'alertaFilters',
+    'alertaServices',
+    'alertaDirectives',
+    'alertaControllers',
+    'satellizer'
+  ])
 
-var alertaApp = angular.module('alertaApp', [
-  'config',
-  'ngRoute',
-  'ngSanitize',
-  'alertaFilters',
-  'alertaServices',
-  'alertaDirectives',
-  'alertaControllers',
-  'satellizer'
-])
+  .config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider
+      .when('/alerts', {
+        templateUrl: 'partials/alert-list.html',
+        controller: 'AlertListController',
+        reloadOnSearch: false
+      })
+      .when('/alert/:id', {
+        templateUrl: 'partials/alert-details.html',
+        controller: 'AlertDetailController'
+      })
+      .when('/top10', {
+        templateUrl: 'partials/alert-top10.html',
+        controller: 'AlertTop10Controller',
+        reloadOnSearch: false
+      })
+      .when('/watch', {
+        templateUrl: 'partials/alert-watch.html',
+        controller: 'AlertWatchController'
+      })
+      .when('/blackouts', {
+        templateUrl: 'partials/blackouts.html',
+        controller: 'AlertBlackoutController'
+      })
+      .when('/users', {
+        templateUrl: 'partials/users.html',
+        controller: 'UserController'
+      })
+      .when('/keys', {
+        templateUrl: 'partials/keys.html',
+        controller: 'ApiKeyController'
+      })
+      .when('/profile', {
+        templateUrl: 'partials/profile.html',
+        controller: 'ProfileController'
+      })
+      .when('/about', {
+        templateUrl: 'partials/about.html',
+        controller: 'AboutController'
+      })
+      .when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'LoginController'
+      })
+      .when('/signup', {
+        templateUrl: 'partials/signup.html',
+        controller: 'SignupController'
+      })
+      .when('/logout', {
+        templateUrl: 'partials/logout.html',
+        controller: 'LogoutController'
+      })
+      .otherwise({
+        redirectTo: '/alerts'
+      });
+   }])
 
-alertaApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider
-    .when('/alerts', {
-      templateUrl: 'partials/alert-list.html',
-      controller: 'AlertListController',
-      reloadOnSearch: false
-    })
-    .when('/alert/:id', {
-      templateUrl: 'partials/alert-details.html',
-      controller: 'AlertDetailController'
-    })
-    .when('/top10', {
-      templateUrl: 'partials/alert-top10.html',
-      controller: 'AlertTop10Controller',
-      reloadOnSearch: false
-    })
-    .when('/watch', {
-      templateUrl: 'partials/alert-watch.html',
-      controller: 'AlertWatchController'
-    })
-    .when('/blackouts', {
-      templateUrl: 'partials/blackouts.html',
-      controller: 'AlertBlackoutController'
-    })
-    .when('/users', {
-      templateUrl: 'partials/users.html',
-      controller: 'UserController'
-    })
-    .when('/keys', {
-      templateUrl: 'partials/keys.html',
-      controller: 'ApiKeyController'
-    })
-    .when('/profile', {
-      templateUrl: 'partials/profile.html',
-      controller: 'ProfileController'
-    })
-    .when('/about', {
-      templateUrl: 'partials/about.html',
-      controller: 'AboutController'
-    })
-    .when('/login', {
-      templateUrl: 'partials/login.html',
-      controller: 'LoginController'
-    })
-    .when('/signup', {
-      templateUrl: 'partials/signup.html',
-      controller: 'SignupController'
-    })
-    .when('/logout', {
-      templateUrl: 'partials/logout.html',
-      controller: 'LogoutController'
-    })
-    .otherwise({
-      redirectTo: '/alerts'
-    });
+  .config(['$httpProvider',
+    function ($httpProvider) {
+      $httpProvider.interceptors.push(function ($q, $location) {
+          return {
+              'response': function (response) {
+                  //Will only be called for HTTP up to 300
+                  return response;
+              },
+              'responseError': function (rejection) {
+                  if(rejection.status === 401) {
+                      $location.path('/login');
+                  }
+                  return $q.reject(rejection);
+              }
+          };
+      });
+  }])
+
+  .config(['config', '$authProvider',
+    function (config, $authProvider) {
+      $authProvider.loginUrl = config.endpoint+'/auth/login';
+      $authProvider.signupUrl = config.endpoint+'/auth/signup';
+      $authProvider.logoutRedirect = '/login';
+      $authProvider.google({
+        url: config.endpoint+'/auth/google',
+        clientId: config.client_id
+      });
+      $authProvider.github({
+        url: config.endpoint+'/auth/github',
+        clientId: config.client_id
+      });
+      $authProvider.twitter({
+        url: config.endpoint+'/auth/twitter',
+        clientId: config.client_id
+      });
   }]);
-
-alertaApp.config(['$httpProvider',
-  function ($httpProvider) {
-    $httpProvider.interceptors.push(function ($q, $location) {
-        return {
-            'response': function (response) {
-                //Will only be called for HTTP up to 300
-                return response;
-            },
-            'responseError': function (rejection) {
-                if(rejection.status === 401) {
-                    $location.path('/login');
-                }
-                return $q.reject(rejection);
-            }
-        };
-    });
-}]);
-
-alertaApp.config(['config', '$authProvider',
-  function (config, $authProvider) {
-    $authProvider.loginUrl = config.endpoint+'/auth/login';
-    $authProvider.signupUrl = config.endpoint+'/auth/signup';
-    $authProvider.logoutRedirect = '/login';
-    $authProvider.google({
-      url: config.endpoint+'/auth/google',
-      clientId: config.client_id
-    });
-    $authProvider.github({
-      url: config.endpoint+'/auth/github',
-      clientId: config.client_id
-    });
-    $authProvider.twitter({
-      url: config.endpoint+'/auth/twitter',
-      clientId: config.client_id
-    });
-}]);
