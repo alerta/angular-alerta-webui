2f8f9b21006440b1143f9dae7b7c74f6aacbeffb
Nick Satterly <nick.satterly@guardian.co.uk>
2015-11-28 20:10:18 +0000

    Remove customer lookup from list of users

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -636,14 +636,13 @@ alertaControllers.controller('AlertBlackoutController', ['$scope', '$route', '$t
   }]);
 
 
-alertaControllers.controller('UserController', ['$scope', '$route', '$timeout', '$auth', 'config', 'Users', 'Customers',
-  function($scope, $route, $timeout, $auth, config, Users, Customers) {
+alertaControllers.controller('UserController', ['$scope', '$route', '$timeout', '$auth', 'config', 'Users',
+  function($scope, $route, $timeout, $auth, config, Users) {
 
     $scope.domains = [];
     $scope.users = [];
     $scope.login = '';
     $scope.provider = config.provider;
-    $scope.customers = {};
 
     switch (config.provider) {
       case "google":
@@ -679,12 +678,6 @@ alertaControllers.controller('UserController', ['$scope', '$route', '$timeout', 
       $scope.orgs = response.orgs;
       $scope.groups = response.groups;
       $scope.users = response.users;
-      angular.forEach($scope.users, function(user) {
-        console.log(user);
-        Customers.lookup({user: user.login}, function(data) {
-          $scope.customers[user] = data.customer;
-        });
-      });
     });
 
   }]);
