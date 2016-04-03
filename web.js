50cf74adaa4ad56eaa5c0ef2a9810f88e0fbff14
Nick Satterly <nick.satterly@guardian.co.uk>
2016-04-03 17:21:22 +0100

    Redirect http to https

--- web.js
+++ web.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+var forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    res.header("Strict-Transport-Security", "max-age=31536000");
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(forceSsl);
 app.get('/config.js', function(request, response) {
 
   var config = " \
@@ -15,8 +26,6 @@ app.get('/config.js', function(request, response) {
 
   response.send(config);
 });
-
-app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/app'));
 
 app.listen(app.get('port'), function() {
