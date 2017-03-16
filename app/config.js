dc9862726a87085895f3ac69fc8c96f1a39b5a19
Matteo Cerutti <matteo.cerutti@rkrepsilon.com>
2017-03-16 15:23:33 +0000

    adding keycloak auth provider support

--- app/config.js
+++ app/config.js
@@ -3,10 +3,12 @@
 angular.module('config', [])
   .constant('config', {
     'endpoint'    : "http://"+window.location.hostname+":8080",
-    'provider'    : "basic", // google, github, gitlab or basic
+    'provider'    : "basic", // google, github, gitlab, keycloak or basic
     'client_id'   : "INSERT-CLIENT-ID-HERE",
     'github_url'  : null,  // replace with your enterprise github server
     'gitlab_url'  : "https://gitlab.com",  // replace with your gitlab server
+    'keycloak_url': "https://keycloak.example.org",  // replace with your keycloak server
+    'keycloak_realm': "master",  // replace with your keycloak realm
     'colors'      : {}, // use default colors
     'severity'    : {}, // use default severity codes
     'audio'       : {}, // no audio
