c2f9f8ac9e4621d9eb96faf9b6072d67b36d310f
Nick Satterly <nick.satterly@theguardian.com>
2018-04-02 17:27:54 +0200

    Add missing comma and fix alignment

--- app/config.js
+++ app/config.js
@@ -2,16 +2,16 @@
 
 angular.module('config', [])
   .constant('config', {
-    'endpoint'    : "http://"+window.location.hostname+":8080",
-    'provider'    : "basic", // google, github, gitlab, keycloak, pingfederate, saml2 or basic
-    'client_id'   : "INSERT-CLIENT-ID-HERE",
-    'github_url'  : null,  // replace with your enterprise github server
-    'gitlab_url'  : "https://gitlab.com",  // replace with your gitlab server
-    'keycloak_url': "https://keycloak.example.org",  // replace with your keycloak server
-    'keycloak_realm': "master",  // replace with your keycloak realm
-    'pingfederate_url' : "https://pingfederate.example.org" // replace with your pingfederate server
-    'colors'      : {}, // use default colors
-    'severity'    : {}, // use default severity codes
-    'audio'       : {}, // no audio
-    'tracking_id' : ""  // Google Analytics tracking ID eg. UA-NNNNNN-N
+    'endpoint'        : "http://"+window.location.hostname+":8080",
+    'provider'        : "basic", // google, github, gitlab, keycloak, pingfederate, saml2 or basic
+    'client_id'       : "INSERT-CLIENT-ID-HERE",
+    'github_url'      : null,  // replace with your enterprise github server
+    'gitlab_url'      : "https://gitlab.com",  // replace with your gitlab server
+    'keycloak_url'    : "https://keycloak.example.org",  // replace with your keycloak server
+    'keycloak_realm'  : "master",  // replace with your keycloak realm
+    'pingfederate_url': "https://pingfederate.example.org", // replace with your pingfederate server
+    'colors'          : {}, // use default colors
+    'severity'        : {}, // use default severity codes
+    'audio'           : {}, // no audio
+    'tracking_id'     : ""  // Google Analytics tracking ID eg. UA-NNNNNN-N
   });
