257bdad2baa3ede2b43e243657487bfc14fd751b
Nick Satterly <nfsatterly@gmail.com>
2019-07-03 00:16:03 +0200

    Update deploy script for config.json

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,13 +10,11 @@ fi
 
 HOSTED_ZONE_ID=Z2RNJ4H6FV67LG
 DOMAIN=try.alerta.io
-GOOGLE_TRACKING_ID=UA-44644195-1
-OAUTH2_CLIENT_ID=736147134702-glkb1pesv716j1utg4llg7c3rr7nnhli.apps.googleusercontent.com
 S3_HOSTED_ZONE_ID=Z1BKCTXD74EZPE
 AWS_DEFAULT_REGION=eu-west-1
 export AWS_DEFAULT_REGION
 
-TMP_CONFIG_JS=/tmp/config.js.$$
+TMP_CONFIG_JSON=/tmp/config.json.$$
 TMP_INDEX_HTML=/tmp/index.html.$$
 TMP_INPUT_JSON=/tmp/route53-change-resource-record-sets.json.$$
 
@@ -32,21 +30,14 @@ aws s3 website s3://${DOMAIN} --index-document index.html
 
 ##### APP CONFIG #####
 
-echo "# Copy update config.js to S3: config.js -> s3://${DOMAIN}/config.js ..."
+echo "# Copy update config.json to S3: config.json -> s3://${DOMAIN}/config.json ..."
 
-cat >${TMP_CONFIG_JS} << EOF
-'use strict';
-angular.module('config', [])
-  .constant('config', {
-    'endpoint'    : "http://alerta-api.herokuapp.com",
-    'provider'    : "google",
-    'client_id'   : "${OAUTH2_CLIENT_ID}",
-    'tracking_id' : "${GOOGLE_TRACKING_ID}"
-  });
+cat >${TMP_CONFIG_JSON} << EOF
+{"endpoint": "https://alerta-api.herokuapp.com"}
 EOF
 
-aws s3 cp ${TMP_CONFIG_JS} s3://${DOMAIN}/config.js --acl public-read --content-type application/javascript
-rm ${TMP_CONFIG_JS}
+aws s3 cp ${TMP_CONFIG_JSON} s3://${DOMAIN}/config.json --acl public-read --content-type application/javascript
+rm ${TMP_CONFIG_JSON}
 
 ##### Route53 #####
 
